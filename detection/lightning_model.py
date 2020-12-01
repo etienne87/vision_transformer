24c6d352b00b12a0edc9ba08dad17c942a8edf72
etienne87 <et.perot@gmail.com>
2020-12-01 20:07:55 +0100

    clean

--- detection/lightning_model.py
+++ detection/lightning_model.py
@@ -18,9 +18,11 @@ from core.temporal import time_to_batch
 from detection.hungarian_loss import HungarianMatcher, SetCriterion
 from detection.utils import normalize, filter_outliers
 from detection.box_ops import box_xyxy_to_cxcywh
+from detection.post_process import PostProcess
+from data import box_api
 
 from types import SimpleNamespace
-from itertools import chain
+from itertools import chain, islice
 from torchvision.utils import make_grid
 
 
@@ -36,6 +38,7 @@ class DetectionModel(pl.LightningModule) :
         self.weight_dict = {'loss_ce': 1, 'loss_bbox': hparams.bbox_loss_coef, 'loss_giou': hparams.giou_loss_coef}
         matcher = HungarianMatcher(hparams.cost_class, hparams.cost_bbox, hparams.cost_giou)
         self.criterion = SetCriterion(self.num_classes, matcher, hparams.eos_coef, losses = ['labels', 'boxes', 'cardinality'])
+        self.post_process = PostProcess()
 
     def _inference(self, batch, batch_nb):
         x, reset_mask = batch["inputs"], batch["mask_keep_memory"] 
@@ -45,6 +48,15 @@ class DetectionModel(pl.LightningModule) :
         out = time_to_batch(out)[0]
         out = {'pred_logits': out[...,4:], 'pred_boxes': out[..., :4].sigmoid()} 
         return out
+    
+    @torch.no_grad()
+    def get_boxes(self, batch, batch_nb):
+        h, w = batch['inputs'].shape[-2:]
+        out = self._inference(batch, batch_nb)
+        target_sizes = torch.FloatTensor([h, w]*len(out)).reshape(len(out), 2)
+        import pdb;pdb.set_trace()
+        boxes = self.post_process(out, target_sizes) 
+        return boxes
 
     def get_loss(self, batch, batch_nb):
         h, w = batch['inputs'].shape[-2:]
@@ -55,10 +67,10 @@ class DetectionModel(pl.LightningModule) :
         out = self._inference(batch, batch_nb)
         loss_dict = self.criterion(out, targets)
         loss = sum([loss_dict[key]*weight for key, weight in self.weight_dict.items()]) 
-        return loss, loss_dict
+        return out, loss, loss_dict
 
     def training_step(self,batch, batch_nb) :
-        loss, loss_dict = self.get_loss(batch, batch_nb)
+        _, loss, loss_dict = self.get_loss(batch, batch_nb)
         for key in self.weight_dict.keys():
             self.log('train_loss_'+key, loss_dict[key])
         self.log('train_loss', loss.item())
@@ -66,10 +78,12 @@ class DetectionModel(pl.LightningModule) :
         
     def validation_step(self, batch, batch_nb):
         with torch.no_grad():
-            loss, loss_dict = self.get_loss(batch, batch_nb)
+            out, loss, loss_dict = self.get_loss(batch, batch_nb)
         for key in self.weight_dict.keys():
             self.log('train_loss_'+key, loss_dict[key])
         self.log('val_loss', loss.item())
+        #TODO
+        #self.accumulate_results(out)
 
     def validation_epoch_end(self, validation_step_outputs) :
         avg_loss = torch.mean(torch.tensor([elt['val_loss'] for elt in validation_step_outputs]))
@@ -80,71 +94,75 @@ class DetectionModel(pl.LightningModule) :
         sch = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max=10)
         return [opt], [sch]
   
-    """
-    TODO
-    def demo_video(self, dataloader, epoch=-1) :
+    @torch.no_grad()
+    def demo_video(self, dataloader, num_batches=100, show_video=False):
+        """
+        This runs our detector on several videos of the testing dataset
+        """
+        hparams = self.hparams
+
+        height, width = hparams.height, hparams.width
+        batch_size = hparams.batch_size
+        nrows = 2 ** ((batch_size.bit_length() - 1) // 2)
+        ncols = int(np.ceil(hparams.batch_size / nrows))
+
+        grid = np.zeros((nrows * hparams.height, ncols * hparams.width, 3), dtype=np.uint8)
+        video_name = os.path.join(hparams.train_dir, 'videos', f'video#-1.mp4')
+
+        dir = os.path.dirname(video_name)
+        if not os.path.isdir(dir):
+            os.mkdir(dir)
+
+        out_video = skvideo.io.FFmpegWriter(video_name, outputdict={'-vcodec': 'libx264', '-preset':'slow'}) 
+
         self.eval()
-        
-        cv2.namedWindow("histos",cv2.WINDOW_NORMAL)
-        
-        nrows = 2 ** ((self.hparams.batch_size.bit_length() - 1) // 2)
-        
-        count=0
-        total_acc = None
+
+        if show_video:
+            window_name = "test epoch {:d}".format(epoch)
+            cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
+        for batch_nb, batch in enumerate(islice(dataloader, num_batches)):
+            images = batch["inputs"].cpu().clone().data.numpy()
+
+            batch["inputs"] = batch["inputs"].to(self.device)
+            batch["mask_keep_memory"] = batch["mask_keep_memory"].to(self.device)
+
+            predictions = self.get_boxes(batch["inputs"], score_thresh=0.5)
+            import pdb;pdb.set_trace()
+
+            for t in range(len(images)):
+                for i in range(len(images[0])):
+                    frame = self.test_data.get_vis_func()(images[t][i])
+                    pred = predictions[t][i]
+                    target = batch["labels"][t][i]
+
+                    if isinstance(target, torch.Tensor):
+                        target = target.cpu().numpy()
+                    if target.dtype.isbuiltin:
+                        target = box_api.box_vectors_to_bboxes(target[:,:4], target[:,4])
+
+                    if pred['boxes'] is not None:
+                        boxes = pred['boxes'].cpu().data.numpy()
+                        labels = pred['labels'].cpu().data.numpy()
+                        scores = pred['scores'].cpu().data.numpy()
+                        bboxes = box_api.box_vectors_to_bboxes(boxes, labels, scores)
+                        frame = box_api.draw_box_events(frame, bboxes, self.label_map, draw_score=True, thickness=2)
+
+                    frame = box_api.draw_box_events(frame, target, self.label_map, force_color=[255,255,255], draw_score=False, thickness=1)
+
+                    y = i // ncols
+                    x = i % ncols
+                    y1, y2 = y*height, (y+1)*height
+                    x1, x2 = x*width, (x+1)*width
+                    grid[y1:y2,x1:x2] = frame
+
+                if show_video:
+                    cv2.imshow('result', grid)
+                    cv2.waitKey(5)
 
 
-        out_name = os.path.join(self.hparams.train_dir, "videos",  "test#"+str(epoch)+".mp4")
+                out_video.writeFrame(frame[...,::-1])
 
-        dirname = os.path.dirname(out_name)
-        if not os.path.isdir(dirname):
-            os.mkdir(dirname)
-
-        out_video = skvideo.io.FFmpegWriter(out_name, outputdict={'-vcodec': 'libx264', '-preset':'slow'}) 
-        
-        with torch.no_grad() :
-            for batch_idx, batch in enumerate(dataloader) :
-                x, y, reset_mask = batch["inputs"], batch["labels"], batch["mask_keep_memory"]
-                
-                x = x.to(self.device)
-                reset_mask = reset_mask.cuda()
-                if hasattr(self.model , 'reset'):
-                    self.model.reset(reset_mask)
-                
-                out = self.model.forward(x) 
-                out = torch.argmax(out,dim=2) 
-                out = out[:,:,None]
-                
-                x = 255*x
-                
-                tbins, batchsize = x.shape[:2]
-                color = (0, 0, 255)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                for t in range(len(x)):
-                    #input
-                    gridx = make_grid(x[t], nrow=nrows).permute(1, 2, 0).cpu().numpy().astype(np.uint8)
-    
-                    #labels
-                    gridt = make_grid(y[t][:,None], nrow=nrows).permute(1, 2, 0).cpu().numpy().copy()
-                    grid_rgb = cv2.applyColorMap((gridt*30)%255, cv2.COLORMAP_RAINBOW)
-                    grid_rgb[gridt==0] = 0
-
-                    #prediction
-                    grid2 = make_grid(out[t], nrow=nrows).permute(1,2,0).cpu().numpy().copy()
-                    mask_background = np.max(grid2,axis=2)==0 # H,W
-                    grid3 = cv2.applyColorMap(grid2.astype(np.uint8)*30, cv2.COLORMAP_RAINBOW)
-                    grid3[mask_background] = 0
-                    final_grid = grid3.astype(np.uint8)                    
-
-                    frame = np.concatenate([gridx, final_grid, grid_rgb], axis=0)
-                    cv2.imshow("histos",frame)
-
-                    out_video.writeFrame(frame[...,::-1])
-                    
-                    key = cv2.waitKey(5)
-                    if key == 27:
-                        break  
-                           
-            cv2.destroyWindow("histos")                 
-            out_video.close()
-    """ 
-    
+        if show_video:
+            cv2.destroyWindow(window_name)
+        out_video.close()
